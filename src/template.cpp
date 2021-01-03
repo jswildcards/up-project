b733e05411921f36057f0acf00db9836aefb5950
jswildcards <js.wildcards@gmail.com>
2021-01-03 14:18:07 +0000

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -1,22 +1,40 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
+#define LL long long
+#define Q queue
+#define S stack
+#define V vector
+#define PQ priority_queue
+#define PQL PQ<LL>
+#define PQLR PQ<LL, V<LL>, greater<LL>>
+#define PB push_back
+#define FI first
+#define SE second
+#define EL '\n'
+#define GSTYL(i) cout << "Case #" << i << ": "
+#define RP(i, a, n) for (LL i = a; i < (n); i++)
+#define RPI(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
+#define FAST_CIN()         \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0)
 using namespace std;
 
-int solution()
+void solve()
 {
-  int result = 0;
-  return result;
 }
 
 int main()
 {
+  FAST_CIN();
   int n;
   cin >> n;
 
-  int result[n];
-  for (int i = 0; i < n; i++)
-    result[i] = solution();
-
-  for (int i = 0; i < n; i++)
-    cout << "Case #" << (i + 1) << ": " << result[i] << endl;
+  RP(i, 0, n)
+  {
+    solve();
+  }
   return 0;
 }
