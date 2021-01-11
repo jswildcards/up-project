ee612a08ca480d2b0a07b323a6da8db7137f32b6
jswildcards <js.wildcards@gmail.com>
2021-01-11 13:17:17 +0000

    up: 11/1/21

---
+++ src/codeforces/344A.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define LL long long
+#define LEN length()
+#define Q queue
+#define S stack
+#define V vector
+#define PQ priority_queue
+#define PQL PQ<LL>
+#define PQLR PQ<LL, V<LL>, greater<LL>>
+#define PB push_back
+#define FI first
+#define SE second
+#define EL '\n'
+#define GSTYL(i) cout << "Case #" << i << ": "
+#define RP(i, a, n) for (LL i = a; i < (LL)(n); i++)
+#define RPI(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
+#define STEP(i, a, n, s) for (LL i = a; i < (LL)(n); i += s)
+#define FAST_CIN()         \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0)
+using namespace std;
+
+void solve()
+{
+  int n, r = 0;
+  string s, p = "";
+  cin >> n;
+  RP(i, 0, n)
+  {
+    cin >> s;
+    if (p.size() <= 0 || s[0] == p[1])
+      r++;
+    p = s;
+  }
+
+  cout << r << EL;
+}
+
+int main()
+{
+  FAST_CIN();
+  solve();
+  return 0;
+}
---
+++ src/codeforces/791A.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define LL long long
+#define LEN length()
+#define Q queue
+#define S stack
+#define V vector
+#define PQ priority_queue
+#define PQL PQ<LL>
+#define PQLR PQ<LL, V<LL>, greater<LL>>
+#define PB push_back
+#define FI first
+#define SE second
+#define EL '\n'
+#define GSTYL(i) cout << "Case #" << i << ": "
+#define RP(i, a, n) for (LL i = a; i < (LL)(n); i++)
+#define RPI(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
+#define STEP(i, a, n, s) for (LL i = a; i < (LL)(n); i += s)
+#define FAST_CIN()         \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0)
+using namespace std;
+
+void solve()
+{
+  int a, b, r = 0;
+  ;
+  cin >> a >> b;
+  while (a <= b)
+  {
+    a *= 3;
+    b *= 2;
+    r++;
+  }
+  cout << r << EL;
+}
+
+int main()
+{
+  FAST_CIN();
+  solve();
+  return 0;
+}
