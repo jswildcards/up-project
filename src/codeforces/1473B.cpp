9feb961c8fc335120c5364cb657c110fc8886e6d
jswildcards <js.wildcards@gmail.com>
2021-01-19 14:42:10 +0000

    up: 19/1/21

---
+++ src/codeforces/1473B.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define LL long long
+#define LEN length()
+#define Q queue
+#define S stack
+#define V vector
+#define PQ priority_queue
+#define PQL PQ<LL>
+#define PQLR PQ<LL, V<LL>, greater<LL>>
+#define PB push_back
+#define FI first
+#define SE second
+#define EL '\n'
+#define GSTYL(i) cout << "Case #" << i << ": "
+#define RP(i, a, n) for (LL i = a; i < (LL)(n); i++)
+#define RPI(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
+#define STEP(i, a, n, s) for (LL i = a; i < (LL)(n); i += s)
+#define FAST_CIN()         \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0)
+using namespace std;
+
+int gcd(int a, int b)
+{
+  if (b == 0)
+    return a;
+  else
+    return gcd(b, a % b);
+}
+
+void solve()
+{
+  string s, t, p = "", q = "";
+  cin >> s >> t;
+
+  int g = s.LEN * t.LEN / gcd(s.LEN, t.LEN);
+  RP(i, 0, g / s.LEN)
+  p += s;
+  RP(i, 0, g / t.LEN)
+  q += t;
+
+  cout << (p == q ? p : "-1") << EL;
+}
+
+int main()
+{
+  FAST_CIN();
+  int n;
+  cin >> n;
+
+  RP(i, 0, n)
+  {
+    solve();
+  }
+  return 0;
+}
