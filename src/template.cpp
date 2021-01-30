858fa4be936cf32c2f3150810b91d4ddc28c6d9d
jswildcards <js.wildcards@gmail.com>
2021-01-30 03:27:36 +0000

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -2,22 +2,18 @@
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-#define LL long long
+#define int long long
 #define LEN length()
-#define Q queue
-#define S stack
 #define V vector
 #define PQ priority_queue
-#define PQL PQ<LL>
-#define PQLR PQ<LL, V<LL>, greater<LL>>
+#define PQI PQ<int>
+#define PQIR PQ<int, V<int>, greater<int>>
 #define PB push_back
-#define FI first
-#define SE second
 #define EL '\n'
 #define GSTYL(i) cout << "Case #" << i << ": "
-#define RP(i, a, n) for (LL i = a; i < (LL)(n); i++)
-#define RPI(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
-#define STEP(i, a, n, s) for (LL i = a; i < (LL)(n); i += s)
+#define RP(i, a, n) for (int i = a; i < (n); i++)
+#define RPV(i, v) for (auto i = (v).begin(); i != (v).end(); i++)
+#define STEP(i, a, n, s) for (int i = a; i < (n); i += s)
 #define FAST_CIN()         \
   ios::sync_with_stdio(0); \
   cin.tie(0);              \
@@ -28,7 +24,7 @@ void solve()
 {
 }
 
-int main()
+int32_t main()
 {
   FAST_CIN();
   int n;
